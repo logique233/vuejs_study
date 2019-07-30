7195c4ad0e188a5ede84a5bb6103c1d603f3f53d
logique233 <2219422983@qq.com>
2019-07-30 18:06:09 +0800

    :bug:

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
 import router from './router'
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+    /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
